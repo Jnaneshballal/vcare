b0d1f93fff0856025bc2d6a5cd67456e2efaae85
akashraj22 <akashraj.jain22@gmail.com>
2020-11-23 14:22:29 +0530

    shopkeeper UI template

---
+++ vcare/shopkeeper/assets/footer.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="container">
+        <div class="text-center">
+          FOOTER<br>
+          Copyright © 2020 Akash Raj for VCare mechanic side
+        </div>
+    </div>
+</footer>
---
+++ vcare/shopkeeper/assets/jslink.php
@@ -0,0 +1,22 @@
+<!-- Bootstrap core JavaScript-->
+<script src="../m_plugins/js/jquery.min.js"></script>
+<script src="../m_plugins/js/popper.min.js"></script>
+<script src="../m_plugins/js/bootstrap.min.js"></script>
+	
+<!-- loader-->
+<script src="../m_plugins/js/pace.min.js"></script>
+
+<!-- simplebar js -->
+<script src="../m_plugins/plugins/simplebar/js/simplebar.js"></script>
+<!-- sidebar-menu js -->
+<script src="../m_plugins/js/sidebar-menu.js"></script>
+<!-- loader scripts -->
+<script src="../m_plugins/js/jquery.loading-indicator.js"></script>
+<!-- Custom scripts -->
+<script src="../m_plugins/js/app-script.js"></script>
+<!-- Chart js -->
+  
+<script src="../m_plugins/plugins/Chart.js/Chart.min.js"></script>
+ 
+<!-- Index js -->
+<script src="../m_plugins/js/index.js"></script>
