0c6afa63ba7f8d416548c24c7c95f679c21d15bc
akashraj22 <akashraj.jain22@gmail.com>
2020-11-24 12:13:06 +0530

    shopregister ui

--- vcare/shopkeeper/assets/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="footer">
-    <div class="container">
-        <div class="text-center">
-          FOOTER<br>
-          Copyright © 2020 Akash Raj for VCare mechanic side
-        </div>
-    </div>
-</footer>
