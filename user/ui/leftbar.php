29408075705aaa8edd42b0375d7175a368d909b5
jnaneshballal <jnanesh024@gmail.com>
2020-11-25 20:10:45 +0530

    user table updated

--- user/ui/leftbar.php
+++ user/ui/leftbar.php
@@ -12,9 +12,9 @@
             <div id="main-menu" class="main-menu collapse navbar-collapse">
                 <ul class="nav navbar-nav">
                     <li class="active">
-                        <a href="#"> <i class="menu-icon fa fa-dashboard"></i>Dashboard </a>
+                        <a href="../index.php"> <i class="menu-icon fa fa-dashboard"></i>Dashboard </a>
                     </li>
-                    <h3 class="menu-title">UI elements</h3><!-- /.menu-title -->
+                    <h3 class="menu-title">Services</h3><!-- /.menu-title -->
                     <!-- <li class="menu-item-has-children dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-laptop"></i>Components</a>
                         <ul class="sub-menu children dropdown-menu">
@@ -40,10 +40,10 @@
                     </li>
                     <li class="menu-item-has-children dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-th"></i>Forms</a>
-                        <ul class="sub-menu children dropdown-menu">
-                            <li><i class="menu-icon fa fa-th"></i><a href="forms-basic.html">Basic Form</a></li>
-                            <li><i class="menu-icon fa fa-th"></i><a href="forms-advanced.html">Advanced Form</a></li>
-                        </ul>
+                        <ul class="sub-menu children dropdown-menu"> -->
+                            <li><a href="./../add-vehicle.php">Add Vehicle</a></li> 
+                            <!-- <li></i><a href="forms-advanced.html">Advanced Form</a></li> -->
+                        <!-- </ul>
                     </li> -->
 
                     <h3 class="menu-title">Icons</h3><!-- /.menu-title -->
