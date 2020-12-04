3b0497b9813bc240b95fb19bb015b3795c060a43
jnaneshballal <jnanesh024@gmail.com>
2020-12-04 13:10:35 +0530

    del vehicle done

--- user/view-vehicle.php
+++ user/view-vehicle.php
@@ -4,8 +4,10 @@ if (!$login) {
     echo "<script>window.location.href='login/login.php'; </script>";
 }
 $fetch_info = mysqli_query($con, "SELECT * FROM vehicle_info WHERE vuid=$global_uid");
-if (isset($_POST['update'])) {
-}
+// if (isset($_POST['ydel'])) {
+//     $vid=$_POST['vhid'];
+//     $del_query=mysqli_query($con,"DELETE FROM vehicle_info WHERE vid=$vid");
+// }
 ?>
 <!doctype html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
@@ -24,6 +26,7 @@ if (isset($_POST['update'])) {
     <div id="right-panel" class="right-panel">
         <?php include 'ui/header.php' ?>
         <?php while ($info = mysqli_fetch_assoc($fetch_info)) {
+
         ?>
             <div class="col-md-4">
                 <aside class="profile-nav alt">
@@ -52,11 +55,11 @@ if (isset($_POST['update'])) {
                                 <i class="fa fa-calendar"></i> Insurance Expire Date <span class="badge badge-danger pull-right"><?php echo $info['vinsureexdate']; ?></span>
                             </li>
                             <li class="list-group-item">
-                                <button type="button" class="btn btn-danger mb-1" data-toggle="modal" data-target="#staticModal" style="border-radius:50px;text-align:center;">
+                                <button type="button" data-vid="<?php echo $info['vid']; ?>" data-vnum="<?php echo $info['vno']; ?>" onclick="delvechicle(this)" class="btn btn-danger mb-1" data-toggle="modal" data-target="#staticModal" style="border-radius:50px;text-align:center;">
                                     <i class="fa fa-trash"></i>
                                 </button>
                                 <!-- <a href="#"><span class="badge badge-warning pull-right r-activity">Edit<i class="fa fa-edit"></i></span></a> -->
-                                <button type="button" class="btn btn-warning pull-right mb-1" data-toggle="modal" data-target="#mediumModal" style="border-radius:50px;text-align:center;">
+                                <button type="button" data-veid="<?php echo $info['vid']; ?>" data-veno="<?php echo $info['vno']; ?>" class="btn btn-warning pull-right mb-1" data-toggle="modal" data-target="#mediumModal" style="border-radius:50px;text-align:center;" onclick="updatevehicle(this)">
                                     <i class="fa fa-edit"></i>
                                 </button>
                             </li>
@@ -66,30 +69,29 @@ if (isset($_POST['update'])) {
                 </aside>
             </div>
         <?php } ?>
-        <form method="POST">
-            <div class="modal fade" id="staticModal" tabindex="-1" role="dialog" aria-labelledby="staticModalLabel" aria-hidden="true" data-backdrop="static">
-                <div class="modal-dialog modal-sm" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="staticModalLabel">DELETE !!</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <p>
-                                Are you sure you want to DELETE the vehicle <?php echo $vno; ?> ?
-                            </p>
-                        </div>
-                        <div class="modal-footer">
+
+        <div class="modal fade" id="staticModal" tabindex="-1" role="dialog" aria-labelledby="staticModalLabel" aria-hidden="true" data-backdrop="static">
+            <div class="modal-dialog modal-sm" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="staticModalLabel">DELETE !!</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <p>
+                            Are you sure you want to DELETE the vehicle <span id="model-vhno"></span>?
+                        </p>
+                        <form method="post">
+                            <input type="hidden" name="vhid" value="" id="model-vid">
                             <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
                             <button type="submit" class="btn btn-success" name="ydel"><i class="fa fa-check"> Yes</i></button>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
-        </form>
-        <form method="POST">
+        </div>
             <div class="modal fade" id="mediumModal" tabindex="-1" role="dialog" aria-labelledby="mediumModalLabel" aria-hidden="true" data-backdrop="static">
                 <div class="modal-dialog modal-lg" role="document">
                     <div class="modal-content">
@@ -114,15 +116,35 @@ if (isset($_POST['update'])) {
                             </div>
                         </div>
                         <div class="modal-footer">
+                            <input type="hidden" name="vhid" value="" id="model-vid">
                             <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
                             <button type="button" class="btn btn-warning" name="update"><i class="fa fa-edit"></i> Update</button>
                         </div>
                     </div>
                 </div>
             </div>
-        </form>
     </div>
     <?php include 'ui/jslink.php' ?>
+
+    <script>
+        function delvechicle(ele) {
+            var vhno = ele.getAttribute("data-vnum");
+            var vhid = ele.getAttribute("data-vid");
+
+            document.getElementById("model-vhno").innerHTML = vhno;
+            document.getElementById("model-vid").value = vhid;
+            //alert(vhno);
+        }
+        function updatevehicle(item)
+        {
+            var veno = ele.getAttribute("data-veno");
+            var veid = ele.getAttribute("data-veid");
+
+            document.getElementById("model-vhno").innerHTML = vhno;
+            document.getElementById("model-vid").value = vhid;
+        }
+    </script>
+
 </body>
 
 </html>
